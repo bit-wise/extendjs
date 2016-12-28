66a8b834140656bc2e7051217191fd9b11821973
Bitwise Studio <nick@nickbar.com>
2016-12-28 06:48:13 -0500

    dot notation for number sum

--- extend/math.js
+++ extend/math.js
@@ -1,12 +1,23 @@
 Math.sum = function (arr) {
     'use strict';
-    var sum = 0;
-    if (typeof arr === "string") {
-        return "its a string";
+    var sum = 0,
+        placeholder,
+        placeholder_high,
+        placeholder_low;
+    if (typeof arr === "string" && arr.match(/\.\./g)) {
+        placeholder = arr.split('..');
+        placeholder_high = placeholder[1];
+        placeholder_low = placeholder[0];
+        while (placeholder_high >= placeholder_low) {
+            sum += Number(placeholder_high);
+            placeholder_high -= 1;
+        }
+    } else if (typeof arr === "string") {
+        sum = Number(arr);
+    } else {
+        arr.map(function (m) {
+            sum += Number(m);
+        });
     }
-    window.console.log(typeof arr);
-    arr.map(function (m) {
-        sum += Number(m);
-    });
     return sum;
 };
