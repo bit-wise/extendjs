e90f7452b4955b8c42d550b9bf6fae948769ce46
Bitwise Studio <nick@nickbar.com>
2016-12-28 12:17:03 -0500

    adds phi

--- extend/math.js
+++ extend/math.js
@@ -1,17 +1,34 @@
+// PHI
+Math.PHI = (1 + Math.sqrt(5)) / 2;
+Math.phi = function (num) {
+    'use strict';
+    var arr = {
+        n: {},
+        r: {}
+    };
+    num = Number(num);
+    arr.n.up = num * Math.PHI;
+    arr.n.down = num / Math.PHI;
+    arr.r.up = Math.round(arr.n.up);
+    arr.r.down = Math.round(arr.n.down);
+    return arr;
+};
+
+// SUM
 Math.sum = function (arr, inc) {
     'use strict';
     var sum = 0,
         placeholder,
         placeholder_high,
         placeholder_low,
-        i = 1e8;
-    inc = inc || 1;
+        i = 1e+8;
+    inc = Number(inc) || 1;
     if (typeof arr === "string" && arr.match(/\.\./g)) {
         placeholder = arr.split('..');
-        placeholder_low = placeholder[0];
-        placeholder_high = placeholder[1];
+        placeholder_low = Number(placeholder[0]);
+        placeholder_high = Number(placeholder[1]);
         while (placeholder_low <= placeholder_high) {
-            sum += Number(placeholder_low);
+            sum = sum + placeholder_low;
             placeholder_low -= -inc;
             i -= 1;
             if (i < 0) {
@@ -27,4 +44,16 @@ Math.sum = function (arr, inc) {
         });
     }
     return sum;
+};
+
+// FACTORIAL
+Math.fact = function (num) {
+    'use strict';
+    num = Number(num);
+    var prod = num;
+    while (num > 1) {
+        num -= 1;
+        prod = prod * num;
+    }
+    return prod;
 };
